1c1bdf3ea1f00680f89ad0ac682fcbbae4c53771
vaishali <guptavaishali628@gmail.com>
2026-01-02 20:18:42 +0530

    apply Delete method crud

--- src/DataShow.jsx
+++ src/DataShow.jsx
@@ -7,23 +7,47 @@ const DataShow = () => {
     let [Data,setData]=useState([]) // jab input lena ho to usestate empty rakhte hai  
 
 // GET method
-    useEffect(()=>{
-       //let api = 'http://localhost:3000/movieTicket'
-       let api = 'http://localhost:3000/movieTicket'
-      
-       //get data with error handling (.then(callBackFunction with event).catch(callBackFunction with event))
-       axios.get(api).then((res)=>{
-        
-        // console.log(res.data)  //data is inbuilt key name
+    // to stop the refreshment of page after delete the data to update ui 
+    let fetchdata=()=>{
+      let api='http://localhost:3000/movieTicket'
+      axios.get(api).then((res)=>{
         setData(res.data)
+      }).catch((err)=>{
+          console.log(err)  //err understand by developer if we want to show err to user to err then replace err to any msg like "page not found"
+      })
+    }
+    useEffect(()=>{
+    //    //let api = 'http://localhost:3000/movieTicket'
+    //    let api = 'http://localhost:3000/movieTicket'
       
-    }).catch((err)=>{
+    //    //get data with error handling (.then(callBackFunction with event).catch(callBackFunction with event))
+    //    axios.get(api).then((res)=>{
         
-        console.log(err)  //err understand by developer if we want to show err to user to err then replace err to any msg like "page not found"
+    //     // console.log(res.data)  //data is inbuilt key name
+    //     setData(res.data)
       
-    })
+    // }).catch((err)=>{
+        
+    //     console.log(err)  //err understand by developer if we want to show err to user to err then replace err to any msg like "page not found"
+      
+    // })
+
+    //call the fetchdata function
+    fetchdata()
      
     },[])   
+//DELETE method
+    //create function 'DeleteData'  (jab hum normal aise krte hai to hme page ko refresh krna pdega tb jake ui pr update hoga iske liye hum fetchdata ko useEffect k bahar likhenege)
+    let DeleteData=(id)=>{
+      let api=`http://localhost:3000/movieTicket/${id}`  //with id to target that obj to delete
+      axios.delete(api).then((res)=>{
+        alert("data deleted!")
+        //call fetch data function
+        fetchdata()
+      }).catch((err)=>{
+        alert("server break so failed to delete data")   
+      })
+    }
 
   return (
     <>
@@ -37,6 +61,8 @@ const DataShow = () => {
               <th>Movie</th>
               <th>Date</th>
               <th>Time</th>
+              {/* for delete */}
+              <th>Delete</th>
             </tr>
         </thead>
         <tbody>
@@ -49,6 +75,8 @@ const DataShow = () => {
                 <td>{e.movie}</td>
                 <td>{e.date}</td>
                 <td>{e.time}</td>
+                {/* for delete */}
+                <td onClick={()=>{DeleteData(e.id)}}>Delete</td>
              </tr>
             ))
         }
